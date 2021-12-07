be524cbc37992b393345f0c4d022e243de987308
Roger <rogelio.g.sandoval@gmail.com>
2021-12-07 17:35:52 -0600

    Create a navbar and apply it to the main

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,38 @@
+<div class="flex flex-wrap place-items-center mx-auto">
+	<section class="relative mx-auto w-full">
+		<!-- navbar -->
+	  <nav class="flex justify-between text-white">
+		<!-- <div class="xl:px-12 flex w-full items-center"> -->
+        <div class="w-full">
+		  <!-- Nav Links -->
+		  <ul class="font-semibold font-heading text-center">
+			<li><a class="button hover:text-gray-400" href="#">Home</a></li>
+			<li><a class="button hover:text-gray-400" href="#">Reviews</a></li>
+			<li><a class="button hover:text-gray-400" href="#">Services</a></li>
+			<li><a class="button hover:text-gray-400" href="#">Careers</a></li>
+			<li><a class="button hover:text-gray-400" href="#">About Us</a></li>
+			<li><a class="button hover:text-gray-400" href="#">FAQs</a></li>
+			<li><a class="button hover:text-gray-400" href="#">Contact Us</a></li>
+		  </ul>
+		</div>
+        <!-- <div class="flex items-center hover:text-gray-200">
+            <img class="h-12" src="../../img/motto.png" alt="motto">
+		</div> -->
+	  </nav>
+	  
+	</section>
+</div>
+
+<style>
+    .button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        border: 2px solid darkolivegreen;
+        background-color: black;
+        padding: 1em;
+        width: 100%;
+        height: 50px;
+        box-sizing: border-box;
+    }
+</style>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,16 @@
 <script>
 	import '../app.css';
+	import Navbar from "$lib/components/navbar.svelte";
 </script>
-
+<div class="nav">
+	<Navbar />
+</div>
 <slot />
+
+<style>
+	@media screen and (max-width:640px) {
+		.nav {
+			display: none;
+		}
+	}
+</style>
