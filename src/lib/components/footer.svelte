b82010081671bbcb862f7419cfbaca0383672c10
Roger <rogelio.g.sandoval@gmail.com>
2021-12-08 15:54:14 -0600

    Add a main footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,62 @@
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+
+    .foot {
+    position:relative;
+    bottom:0;
+    width:100%;
+    }
+
+    .fa {
+    padding: 10px;
+    font-size: 25px;
+    width: 45px;
+    text-align: center;
+    text-decoration: none;
+    margin: 10px 15px;
+    }
+
+    .fa:hover {
+    opacity: 0.7;
+    }
+
+    .fa-facebook {
+    background: #3B5998;
+    color: white;
+    }
+
+    .fa-instagram {
+    background: #125688;
+    color: white;
+    }
+
+    .fa-youtube {
+    background: #bb0000;
+    color: white;
+    }
+    
+</style>
+
+<footer class="foot relative bg-gray-300 p-1">
+  <div class="container mx-auto px-4">
+    <div class="flex flex-wrap text-center">
+      <div class="w-full lg:w-12 px-4">
+        <a href="#" class="fa fa-facebook"></a>
+        <a href="#" class="fa fa-instagram"></a>
+        <a href="#" class="fa fa-youtube"></a>
+      </div>
+    </div>
+    
+    <hr class="my-1 border-blueGray-300">
+    <div class="flex flex-wrap items-center md:justify-between justify-center">
+      <div class="w-full md:w-4/12 px-4 mx-auto text-center">
+        <div class="text-sm text-blueGray-500 font-semibold py-1">
+          Copyright © <span id="get-current-year">2021 </span><a href="#" class="text-blue-400 hover:text-gray-800">Rojaa</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
