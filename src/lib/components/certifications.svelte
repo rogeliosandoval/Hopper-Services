302383d32dc6608294bbf05c0bfb968a8cf84a7c
Roger <rogelio.g.sandoval@gmail.com>
2021-12-08 15:55:20 -0600

    Add certification component

---
+++ src/lib/components/certifications.svelte
@@ -0,0 +1,85 @@
+<div class="mx-auto text-center py-2 sm:py-6 lg:py-12 font-sans">
+    <p class="title tracking-widest bg-green-500 p-4">Our Certifications</p>
+</div>
+
+<div class="container mx-auto">
+  <div class="certBox grid grid-cols-5 gap-4 text-center p-5">
+
+    <div class="mx-auto">
+      <img alt="cert1" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert2" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert3" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert4" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert5" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert6" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert7" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert8" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert9" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert10" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert11" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert12" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert13" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert14" src="cert.png">
+    </div>
+
+    <div class="mx-auto">
+      <img alt="cert15" src="cert.png">
+    </div>
+
+  </div>
+</div>
+
+<style>
+    .certBox {
+        color: green;
+        /* background-image: url("wood.jpg"); */
+        /* background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover; */
+    }
+
+    .title {
+        font-weight: bold;
+        font-size: 15px;
+        font-family: Verdana, sans-serif;
+    }
+</style>
