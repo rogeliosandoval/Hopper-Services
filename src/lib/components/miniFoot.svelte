e1e25586e5a474125081620509eecfb271646e7a
Roger <rogelio.g.sandoval@gmail.com>
2021-12-08 15:52:01 -0600

    Create nav and foot components

---
+++ src/lib/components/miniFoot.svelte
@@ -0,0 +1,35 @@
+<style>
+    .fa {
+        padding: 10px;
+        font-size: 25px;
+        width: 45px;
+        text-align: center;
+        text-decoration: none;
+        margin: 10px 15px;
+    }
+
+    .fa:hover {
+        opacity: 0.7;
+    }
+
+    .fa-facebook {
+        background: #3B5998;
+        color: white;
+    }
+
+    .fa-instagram {
+        background: #125688;
+        color: white;
+    }
+
+    .fa-youtube {
+        background: #bb0000;
+        color: white;
+    }
+</style>
+
+<div class="grid grid-cols-3 bg-gray-700">
+    <div class="text-center"><a href="#" class="fa fa-facebook"></a></div>
+    <div class="text-center"><a href="#" class="fa fa-instagram"></a></div>
+    <div class="text-center"><a href="#" class="fa fa-youtube"></a></div>
+</div>
---
+++ src/lib/components/miniNav.svelte
@@ -0,0 +1,111 @@
+<script>
+    let isActive = false;
+</script>
+
+<style>
+    @media screen and (min-width:640px) {
+        .miniNav {
+            display: none;
+        }
+    }
+
+    @media screen and (min-width:0px) and (max-width:640px) {
+        .miniNav {
+            position: fixed;
+            z-index: 1;
+            top: 0px;
+            width: 100%;
+        }
+    }
+
+    .modGrid {
+        display: table;
+        table-layout: auto;
+        width: 100%;
+    }
+
+    .expand {
+        background-image: linear-gradient(#4CAF50,green);
+        display: table-cell;
+        width: 100%;
+        max-width: 0;
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .shrink {
+        display: table-cell;
+        white-space: nowrap;
+    }
+
+    .logo {
+        /* position:fixed; top:0; right:0; */
+		height: 175px;
+		width: 145px;
+		padding: 1px;
+		position: absolute;
+		margin-top: -75px;
+        margin-left: 80px;
+    }
+
+    .dropbtn {
+        background-image: linear-gradient(#4CAF50,green);
+        color: white;
+        padding: 16px;
+        font-size: 40px;
+        border: none;
+        cursor: pointer;
+    }
+    
+    .dropbtn:hover, .dropbtn:focus {
+        background-color: #3e8e41;
+    }
+    
+    .dropdown {
+        position: relative;
+        display: inline-block;
+    }
+    
+    .dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: #f9f9f9;
+        min-width: 160px;
+        overflow: auto;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+    }
+    
+    .dropdown-content a {
+        color: black;
+        padding: 12px 16px;
+        text-decoration: none;
+        display: block;
+    }
+    
+    .dropdown a:hover {background-color: #f1f1f1}
+    
+    .show {display:block;}
+</style>
+
+<div class="miniNav">
+    <div class="modGrid">
+        <div class="dropdown shrink">
+            <button on:click={() => isActive = !isActive} class="dropbtn"><i class="fa fa-navicon"></i></button>
+            <div id="myDropdown" on:click={() => isActive = !isActive} class:show={isActive} class="dropdown-content">
+                <a href="#">Home</a>
+                <a href="#">Reviews</a>
+                <a href="#">Services</a>
+                <a href="#">Careers</a>
+                <a href="#">About</a>
+                <a href="#">FAQ</a>
+                <a href="#">Contact</a>
+            </div>
+        </div>
+    
+        <!-- <div class="text-white bg-green-800 font-bold text-3xl text-center tracking-wider expand">Hopper Services</div> -->
+        <div class="expand">
+            <img class="logo" alt="logo" src="logo.png">
+        </div>
+    </div>
+</div>
