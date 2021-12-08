5be464aad369acef659454343ce0359690109efb
Roger <rogelio.g.sandoval@gmail.com>
2021-12-08 15:54:57 -0600

    Import the footer

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import '../app.css';
 	import Navbar from "$lib/components/navbar.svelte";
+	import Footer from "$lib/components/footer.svelte";
 </script>
 <div class="nav">
 	<Navbar />
