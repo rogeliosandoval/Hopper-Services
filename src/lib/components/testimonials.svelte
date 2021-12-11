822144a3d55f5c7bdfb5f7db2ac7125963e1d86c
Roger <rogelio.g.sandoval@gmail.com>
2021-12-10 19:14:31 -0600

    Add two more components

---
+++ src/lib/components/testimonials.svelte
@@ -0,0 +1,55 @@
+<script></script>
+
+<style>
+
+.avatar {
+    vertical-align: middle;
+    background: lightblue;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+}
+
+.bio {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: Verdana, sans-serif;
+    background: rgb(228, 228, 228);
+    font-size: 18px;
+    width: 650px;
+    height: 150px;
+}
+
+</style>
+
+<div class="flex flex-row flex-wrap">
+    <div class="flex gap-x-4 pb-10">
+        <img class="avatar" alt="Avatar" src="avatar1.png">
+        <div class="bio px-10 bg-white rounded-2xl">
+            "Hopper Services transformed my yard for the better. I am more than pleased with the service they provided for me and my family! I am proud to say that Hopper Services is our family's goto for lawn care."
+        </div>
+    </div>
+
+    <div class="flex gap-x-4 pb-10">
+        <img class="avatar" alt="Avatar" src="avatar2.png">
+        <div class="bio px-10 bg-white rounded-2xl">
+            "Great service all the way around. These guys know what they are doing and are the REAL deal."
+        </div>
+    </div>
+
+    <div class="flex gap-x-4 pb-10">
+        <img class="avatar" alt="Avatar" src="avatar3.png">
+        <div class="bio px-10 bg-white rounded-2xl">
+            "I didnt think my backyard was salvagable by any means but Hopper Services really proved me wrong. Met the owner and he's a really down to earth guy. My house looks so much better now that my yard is trimmed to my liking. Thank you!"
+        </div>
+    </div>
+
+    <div class="flex gap-x-4">
+        <img class="avatar" alt="Avatar" src="avatar4.png">
+        <div class="bio px-10 bg-white rounded-2xl">
+            "I own an apartment complex and Hopper Services is now my main lawn care service for my complex. They do phenomenal work and I would highly reccomend them to anyone who is looking to keep their nature alive."
+        </div>
+    </div>
+
+</div>
