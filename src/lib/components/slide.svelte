22058b7001642d1a5a0b776f30948669a451e141
Roger <rogelio.g.sandoval@gmail.com>
2021-12-13 12:33:08 -0600

    Add more responsiveness to the slide component

--- src/lib/components/slide.svelte
+++ src/lib/components/slide.svelte
@@ -8,6 +8,104 @@
 </script>
 
 <style>
+
+@media screen and (min-width:0px) and (max-width:768px) {
+    /* Hide the images by default */
+    .mySlides {
+        display: none;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+        color: #f2f2f2;
+        position: absolute;
+        font-size: 12px;
+        padding: 8px 12px;
+        top:0;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    .show {
+        display: block;
+    }
+}
+
+@media screen and (min-width:768px) and (max-width:1024px) {
+    /* Hide the images by default */
+    .mySlides {
+        display: none;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+        color: #f2f2f2;
+        position: absolute;
+        font-size: 12px;
+        padding: 8px 12px;
+        top:0;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    .show {
+        display: block;
+    }
+}
+
+@media screen and (min-width:1024px) and (max-width:1280px) {
+    /* Hide the images by default */
+    .mySlides {
+        display: none;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+        color: #f2f2f2;
+        position: absolute;
+        font-size: 12px;
+        padding: 8px 12px;
+        top:0;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    .show {
+        display: block;
+    }
+}
+
+@media screen and (min-width:1280px) and (max-width:1536px) {
+        /* Hide the images by default */
+        .mySlides {
+        display: none;
+    }
+
+    /* Number text (1/3 etc) */
+    .numbertext {
+        color: #f2f2f2;
+        position: absolute;
+        font-size: 12px;
+        padding: 8px 12px;
+        top:0;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    .show {
+        display: block;
+    }
+}
+
+@media screen and (min-width:1536px) {
     /* Hide the images by default */
     .mySlides {
         display: none;
@@ -29,6 +127,7 @@
     .show {
         display: block;
     }
+}
 
 </style>
 
