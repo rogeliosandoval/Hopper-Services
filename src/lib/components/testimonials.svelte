bd3b03257a05985fd1cb7fdb38c76a921ffcd90d
Roger <rogelio.g.sandoval@gmail.com>
2021-12-13 12:31:33 -0600

    Make testimonial component responsive

--- src/lib/components/testimonials.svelte
+++ src/lib/components/testimonials.svelte
@@ -2,15 +2,100 @@
 
 <style>
 
-.avatar {
+@media screen and (min-width:0px) and (max-width:768px) {
+    .bio {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: Verdana, sans-serif;
+    background: rgb(228, 228, 228);
+    font-size: 15px;
+    width: 100vw;
+    height: 175px;
+    box-shadow: 0 10px 15px black;
+    }
+
+    .avatar {
     vertical-align: middle;
     background: lightblue;
     width: 150px;
     height: 150px;
     border-radius: 50%;
+    box-shadow: 0 10px 15px black;
+    }
 }
 
-.bio {
+@media screen and (min-width:768px) and (max-width:1024px) {
+    .bio {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: Verdana, sans-serif;
+    background: rgb(228, 228, 228);
+    font-size: 15px;
+    width: 600px;
+    height: 175px;
+    box-shadow: 0 10px 15px black;
+    }
+
+    .avatar {
+    vertical-align: middle;
+    background: lightblue;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    box-shadow: 0 10px 15px black;
+    }
+}
+
+@media screen and (min-width:1024px) and (max-width:1280px) {
+    .bio {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: Verdana, sans-serif;
+    background: rgb(228, 228, 228);
+    font-size: 15px;
+    width: 600px;
+    height: 175px;
+    box-shadow: 0 10px 15px black;
+    }
+
+    .avatar {
+    vertical-align: middle;
+    background: lightblue;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    box-shadow: 0 10px 15px black;
+    }
+}
+
+@media screen and (min-width:1280px) and (max-width:1536px) {
+    .bio {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-family: Verdana, sans-serif;
+    background: rgb(228, 228, 228);
+    font-size: 15px;
+    width: 600px;
+    height: 175px;
+    box-shadow: 0 10px 15px black;
+    }
+
+    .avatar {
+    vertical-align: middle;
+    background: lightblue;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    box-shadow: 0 10px 15px black;
+    }
+}
+
+@media screen and (min-width:1536px) {
+    .bio {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -19,36 +104,53 @@
     font-size: 18px;
     width: 650px;
     height: 150px;
+    }
+
+    .avatar {
+    vertical-align: middle;
+    background: lightblue;
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+}
 }
 
 </style>
 
-<div class="flex flex-row flex-wrap">
-    <div class="flex gap-x-4 pb-10">
-        <img class="avatar" alt="Avatar" src="avatar1.png">
-        <div class="bio px-10 bg-white rounded-2xl">
-            "Hopper Services transformed my yard for the better. I am more than pleased with the service they provided for me and my family! I am proud to say that Hopper Services is our family's goto for lawn care."
+<div class="flex flex-row md:flex-col flex-wrap pt-10">
+    <div class="flex flex-col md:flex-row gap-x-4 pb-10 md:mx-auto">
+        <img class="avatar mx-auto" alt="Avatar" src="avatar1.png">
+        <div class="pt-5 lg:pt-0">
+            <div class="bio px-10 bg-white rounded-2xl">
+                "Hopper Services transformed my yard for the better. I am more than pleased with the service they provided for me and my family! I am proud to say that Hopper Services is our family's goto for lawn care."
+            </div>
         </div>
     </div>
 
-    <div class="flex gap-x-4 pb-10">
-        <img class="avatar" alt="Avatar" src="avatar2.png">
-        <div class="bio px-10 bg-white rounded-2xl">
-            "Great service all the way around. These guys know what they are doing and are the REAL deal."
+    <div class="flex flex-col md:flex-row gap-x-4 pb-10 md:mx-auto">
+        <img class="avatar mx-auto" alt="Avatar" src="avatar2.png">
+        <div class="pt-5 md:pt-0">
+            <div class="bio px-10 bg-white rounded-2xl">
+                "Great service all the way around. These guys know what they are doing and are the REAL deal."
+            </div>
         </div>
     </div>
 
-    <div class="flex gap-x-4 pb-10">
-        <img class="avatar" alt="Avatar" src="avatar3.png">
-        <div class="bio px-10 bg-white rounded-2xl">
-            "I didnt think my backyard was salvagable by any means but Hopper Services really proved me wrong. Met the owner and he's a really down to earth guy. My house looks so much better now that my yard is trimmed to my liking. Thank you!"
+    <div class="flex flex-col md:flex-row gap-x-4 pb-10 md:mx-auto">
+        <img class="avatar mx-auto" alt="Avatar" src="avatar3.png">
+        <div class="pt-5 md:pt-0">
+            <div class="bio px-10 bg-white rounded-2xl">
+                "I didnt think my backyard was salvagable by any means but Hopper Services really proved me wrong. Met the owner and he's a really down to earth guy. My house looks so much better now that my yard is trimmed to my liking. Thank you!"
+            </div>
         </div>
     </div>
 
-    <div class="flex gap-x-4">
-        <img class="avatar" alt="Avatar" src="avatar4.png">
-        <div class="bio px-10 bg-white rounded-2xl">
-            "I own an apartment complex and Hopper Services is now my main lawn care service for my complex. They do phenomenal work and I would highly reccomend them to anyone who is looking to keep their nature alive."
+    <div class="flex flex-col md:flex-row gap-x-4 md:mx-auto">
+        <img class="avatar mx-auto" alt="Avatar" src="avatar4.png">
+        <div class="pt-5 md:pt-0">
+            <div class="bio px-10 bg-white rounded-2xl">
+                "I own an apartment complex and Hopper Services is now my main lawn care service for my complex. They do phenomenal work and I would highly reccomend them to anyone who is looking to keep their nature alive."
+            </div>
         </div>
     </div>
 
